34b8fca5be6b533e719c9088825b9dccce42573f
Roman Titov <romantitov@MBP-Roman.Dlink>
2023-11-06 21:33:46 +0300

    enrich readme

--- app/credit.rb
+++ app/credit.rb
@@ -24,7 +24,7 @@ class Credit < Product
     @debt -= amount
     puts "Вы внесли #{amount} рублей по кредиту №#{id}. Сумма долга #{debt} рублей"
 
-    payment = (amount / period.to_f).round + (amount * rate / 100.0 / 12.0).round
+    payment = (debt / period.to_f).round + (debt * rate / 100.0 / 12.0).round
     puts "Ежемесячный платёж уменьшился и стал #{payment} рублей"
   end
 
