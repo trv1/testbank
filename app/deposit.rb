7097dc0d7a5e790a244ccb8e85ac04b3df89ba99
ivanstiv <albertduf@mail.ru>
2023-11-25 17:00:14 +0300

    sas

--- app/deposit.rb
+++ app/deposit.rb
@@ -7,6 +7,7 @@ class Deposit < Product
     super(first_name,last_name)
     @rate = rate
     @period = period
+    @balance = amount
     self
   end
 
--- app/product.rb
+++ app/product.rb
@@ -3,9 +3,9 @@ class Product
 
   attr_accessor :id, :first_name, :last_name, :status, :balance
   def open(first_name,last_name)
-  	@first_name = first_name
+    @first_name = first_name
     @last_name = last_name
     @id = "%06d" % rand(1e6)
     @status = :opened
-end
+  end
 end
