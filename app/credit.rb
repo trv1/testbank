f1117980063f841a669187c08165c5404646b582
ivanstiv <albertduf@mail.ru>
2023-12-03 21:59:45 +0300

    sas

--- app/credit.rb
+++ app/credit.rb
@@ -10,15 +10,15 @@ class Credit < Product
     self
   end
 
-  def payment
+  def monthly_payment
     payment = (balance / period.to_f).round + (balance * rate / 100.0 / 12.0).round
-  
     puts "Ваш ежемесячный платёж составляет #{payment} рублей"
   end
 
   def pay_to_debt(amount)
     @balance -= amount
     puts "Вы внесли #{amount} рублей по кредиту №#{id}. Сумма долга #{balance} рублей"
+    payment = (balance / period.to_f).round + (balance * rate / 100.0 / 12.0).round
     puts "Ежемесячный платёж уменьшился и стал #{payment} рублей"
   end
 
